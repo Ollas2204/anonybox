1804628aeba7ce04929ab109a79ca6c4663779f2
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-05-22 17:58:03 +0700

    seed for posts

---
+++ seeders/20180522102422-Posts.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const fs = require('fs')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    let PostData = fs.readFileSync('post.csv','utf8').split('\n')
+    let postArray = []
+
+    PostData.forEach(element => {
+      element = element.split(',')
+      let newObj = {
+        UserId:element[0],
+        content:element[1],
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      postArray.push(newObj)
+    });
+
+    return queryInterface.bulkInsert('Posts', postArray , {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};

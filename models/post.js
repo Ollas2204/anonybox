72f013e1579adfa1c5bd2d6b4ba1323dafcf25e7
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-05-23 19:46:10 +0700

    tambahin menu delete dan edit post untuk id yang login

--- models/post.js
+++ models/post.js
@@ -3,7 +3,18 @@ module.exports = (sequelize, DataTypes) => {
   var Post = sequelize.define('Post', {
     UserId: DataTypes.INTEGER,
     content: DataTypes.TEXT
-  }, {});
+  }, {
+    hooks:{
+      beforeCreate:(post,options)=>{
+        let contents = post.content.split('\n')
+        for(let i = 0; i < contents.length;i++){
+          contents[i] =  `<p>${contents[i]}</p>`
+        }
+        console.log(contents)
+        post.content = contents.join('')
+      }
+    }
+  });
   
   Post.associate = function(models) {
     Post.belongsToMany(models.User, {
